3681ca2311c854053ca173b2ca3bf9b13be07417
joshbrod <joshbrod@umich.edu>
2023-04-06 11:24:22 -0400

    nationality search

--- HW7.py
+++ HW7.py
@@ -74,7 +74,12 @@ def make_players_table(data, cur, conn):
         # the player's name, their position_id, and their nationality.
 
 def nationality_search(countries, cur, conn):
-    pass
+    valid = []
+    for country in countries:
+        cur.execute("SELECT name,position_id,nationality FROM players WHERE nationality = ?", (country,))
+        valid.extend(cur.fetchall())
+    return valid
+
 
 ## [TASK 3]: 10 points
 # finish the function birthyear_nationality_search
